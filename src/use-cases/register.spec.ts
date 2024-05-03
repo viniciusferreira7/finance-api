14d62376f72df6521630df2a083e861ae85fab2d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-03 09:55:17 -0300

    test: adjusts in the register test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,12 +1,12 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { describe, expect, it } from 'vitest'
-import { RegisterUseCase } from './register'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
 import { UserAlreadyExists } from './error/user-already-exists-error'
+import { RegisterUseCase } from './register'
 
 describe('Register use case', () => {
   it('should be able to hash password upon registration', async () => {
-    const usersRepository = new PrismaUsersRepository()
+    const usersRepository = new InMemoryUsersRepository()
     const sut = new RegisterUseCase(usersRepository)
 
     const password = '123456'
@@ -26,16 +26,16 @@ describe('Register use case', () => {
   })
 
   it('should not be able to register with same email twice', async () => {
-    const usersRepository = new PrismaUsersRepository()
+    const usersRepository = new InMemoryUsersRepository()
     const sut = new RegisterUseCase(usersRepository)
 
     const email = 'john.doe@example.com'
 
-    // await sut.execute({
-    //   name: 'John',
-    //   email,
-    //   password: '12345',
-    // })
+    await sut.execute({
+      name: 'John',
+      email,
+      password: '12345',
+    })
 
     await expect(() =>
       sut.execute({
