3c481278961f4599820a43fbc1e833a1514db181
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-09 09:49:56 -0300

    docs: creates a documentation for users routes

--- src/http/controllers/users/profile.ts
+++ src/http/controllers/users/profile.ts
@@ -1,16 +1,23 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+import { ResourceNotFound } from '@/use-cases/error/resource-not-found-error'
 import { makeGetUserProfileUseCase } from '@/use-cases/factories/users/make-get-user-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  const getUserProfileUseCase = makeGetUserProfileUseCase()
+  try {
+    const getUserProfileUseCase = makeGetUserProfileUseCase()
 
-  const { user } = await getUserProfileUseCase.execute({
-    userId: request.user.sub,
-  })
+    const { user } = await getUserProfileUseCase.execute({
+      userId: request.user.sub,
+    })
 
-  return reply.status(200).send({
-    ...user,
-    password_hash: undefined,
-  })
+    return reply.status(200).send({
+      ...user,
+      password_hash: undefined,
+    })
+  } catch (err) {
+    if (err instanceof ResourceNotFound) {
+      return reply.status(404).send({ message: err.message })
+    }
+  }
 }
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -7,9 +7,111 @@ import { profile } from './profile'
 import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post('/users', register)
-  app.post('/sessions', authenticate)
+  app.post(
+    '/users',
+    {
+      schema: {
+        summary: 'Create a user',
+        description: 'Create a new user',
+        tags: ['User'],
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string', description: 'name of user' },
+            email: { type: 'string', description: 'email of user' },
+            password: { type: 'string', description: 'password of user' },
+          },
+        },
+        response: {
+          201: {
+            description: 'User has been successfully created',
+            type: 'null',
+          },
+          409: {
+            description: 'E-mail already exists',
+            type: 'object',
+            properties: {
+              message: {
+                type: 'string',
+                description: 'E-mail already exists',
+                default: 'E-mail already exists',
+              },
+            },
+          },
+        },
+      },
+    },
+    register,
+  )
+  app.post(
+    '/sessions',
+    {
+      schema: {
+        summary: 'Create a session',
+        description: 'Create a new session for user',
+        tags: ['User'],
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string', description: 'name of user' },
+            email: { type: 'string', description: 'email of user' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Session has been successfully created',
+            type: 'null',
+          },
+          409: {
+            description: 'Invalid credentials.',
+            type: 'object',
+            properties: {
+              message: {
+                type: 'string',
+                description: 'Invalid credentials.',
+                default: 'Invalid credentials.',
+              },
+            },
+          },
+        },
+      },
+    },
+    authenticate,
+  )
 
   // Authenticate
-  app.get('/me', { onRequest: [verifyJWT] }, profile)
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJWT],
+      schema: {
+        summary: 'Get profile',
+        description: 'Get profile of user',
+        tags: ['User'],
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              email: { type: 'string' },
+              created_at: { type: 'string', format: 'date-time' },
+            },
+          },
+          404: {
+            description: 'Resource not found',
+            type: 'object',
+            properties: {
+              message: {
+                type: 'string',
+                description: 'Resource not found',
+                default: 'Resource not found',
+              },
+            },
+          },
+        },
+      },
+    },
+    profile,
+  )
 }
