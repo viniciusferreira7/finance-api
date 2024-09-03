7ede41c4941ad5d3839f52f79b7c613e087b2559
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-03 09:38:43 -0300

    chore: create the workflow to run the E2E tests in the CI on github

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,21 +1,65 @@
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333),
-  DATABASE_USERNAME: z.string(),
-  DATABASE_PASSWORD: z.string(),
-  DATABASE_NAME: z.string(),
-  DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string(),
-})
+export const envSchema = z
+  .object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev').optional(),
+    PORT: z.coerce.number().default(3333).optional(),
+    DATABASE_USERNAME: z.string().optional(),
+    DATABASE_PASSWORD: z.string().optional(),
+    DATABASE_NAME: z.string().optional(),
+    DATABASE_URL: z.string().url().optional(),
+    JWT_SECRET: z.string(),
+  })
+  .superRefine((data, ctx) => {
+    if (process.env.CI === 'true') {
+      return
+    }
+
+    if (!data.DATABASE_USERNAME) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'DATABASE_USERNAME is required',
+        path: ['DATABASE_USERNAME'],
+      })
+    }
+
+    if (!data.DATABASE_PASSWORD) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'DATABASE_PASSWORD is required',
+        path: ['DATABASE_PASSWORD'],
+      })
+    }
+
+    if (!data.DATABASE_NAME) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'DATABASE_NAME is required',
+        path: ['DATABASE_NAME'],
+      })
+    }
+
+    if (!data.DATABASE_URL) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'DATABASE_URL is required',
+        path: ['DATABASE_URL'],
+      })
+    }
+    if (!data.JWT_SECRET) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'DATABASE_URL is required',
+        path: ['DATABASE_URL'],
+      })
+    }
+  })
 
 const _env = envSchema.safeParse(process.env)
 
-if (!_env.success) {
+if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
-
-  throw Error('Invalid environment variables')
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
