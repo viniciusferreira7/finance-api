baaea327cfb0148fc060a5c67fac7995a34fb946
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-07 13:25:17 -0300

    feat: creates auxiliary functions for the incomes and categories test

---
+++ src/utils/test/generate-categories.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker'
+
+import { prisma } from '@/lib/prisma'
+
+interface GenerateCategories {
+  userId: string
+  amount: number
+}
+
+export async function generateCategories({
+  userId,
+  amount,
+}: GenerateCategories) {
+  const categories = Array.from({ length: amount }).map(() => {
+    return {
+      name: faker.lorem.words({ min: 1, max: 5 }),
+      description: faker.lorem.lines({ min: 1, max: 4 }),
+      created_at: faker.date.recent({ days: 120 }),
+      user_id: userId,
+    }
+  })
+
+  await prisma.category.createMany({
+    data: categories,
+  })
+
+  const categoriesCreated = await prisma.category.findMany({
+    where: {
+      user_id: userId,
+    },
+  })
+
+  return categoriesCreated
+}
---
+++ src/utils/test/generate-incomes.ts
@@ -0,0 +1,129 @@
+import { faker } from '@faker-js/faker'
+import { Category, Income, IncomeHistory } from '@prisma/client'
+import { randomUUID } from 'crypto'
+
+import { prisma } from '@/lib/prisma'
+
+import { convertToCents } from '../convert-to-cents'
+import { generateCategories } from './generate-categories'
+
+interface GenerateIncomes {
+  userId: string
+  amount: number
+  withCategory: boolean
+  withHistory: boolean
+}
+
+export async function generateIncomes({
+  userId,
+  amount,
+  withCategory,
+  withHistory,
+}: GenerateIncomes) {
+  const incomes: Income[] = []
+  const incomeHistories: IncomeHistory[] = []
+  const categories: Category[] = []
+
+  if (withCategory) {
+    const prismaCategories = await generateCategories({
+      userId,
+      amount: Math.round(amount / 2),
+    })
+
+    categories.push(...prismaCategories)
+  }
+
+  for (let i = 0; i <= amount; i++) {
+    const incomeId = randomUUID()
+
+    const description = faker.helpers.arrayElement([
+      faker.lorem.lines({ min: 1, max: 10 }),
+      faker.lorem.lines({ min: 5, max: 15 }),
+      null,
+    ])
+
+    const createdAt = faker.date.recent({ days: 120 })
+    const updatedAt = faker.helpers.arrayElement([
+      createdAt,
+      faker.date.recent({ days: 100 }),
+      faker.date.recent({ days: 45 }),
+    ])
+
+    const categoryId = categories.length
+      ? faker.helpers.arrayElement([...categories.map((item) => item.id)])
+      : null
+
+    const income: Income = {
+      id: incomeId,
+      name: faker.finance.accountName(),
+      value: convertToCents(
+        Number(faker.finance.amount({ min: 10, max: 1_000_000 })),
+      ),
+      description,
+      user_id: userId,
+      category_id: categoryId,
+      created_at: createdAt,
+      updated_at: updatedAt,
+    }
+
+    incomes.push(income)
+  }
+
+  await prisma.income.createMany({
+    data: incomes,
+  })
+
+  if (withHistory) {
+    function getCategoriesAndGenerateCategoryId() {
+      const categoryId = faker.helpers.arrayElement([
+        ...categories.map((category) => category.id),
+        ...categories.map((category) => category.id),
+        null,
+      ])
+
+      return categoryId
+    }
+
+    incomes.forEach((income) => {
+      function generateIncomeHistory() {
+        const updateDescription = faker.helpers.arrayElement([
+          faker.lorem.lines({ min: 1, max: 10 }),
+          faker.lorem.lines({ min: 10, max: 15 }),
+          null,
+        ])
+
+        const incomeHistory: IncomeHistory = {
+          id: randomUUID(),
+          name: faker.finance.accountName(),
+          value: convertToCents(
+            Number(faker.finance.amount({ min: 10, max: 1_000_000 })),
+          ),
+          description: updateDescription,
+          user_id: userId,
+          category_id: getCategoriesAndGenerateCategoryId(),
+          income_id: income.id,
+          created_at: income.updated_at,
+        }
+
+        return incomeHistory
+      }
+
+      const historyRange = faker.helpers.arrayElements(
+        Array.from({ length: 45 }),
+        {
+          min: 5,
+          max: 35,
+        },
+      )
+
+      historyRange.forEach(() => {
+        if (income.created_at !== income.updated_at) {
+          incomeHistories.push(generateIncomeHistory())
+        }
+      })
+    })
+    await prisma.incomeHistory.createMany({
+      data: incomeHistories,
+    })
+  }
+}
