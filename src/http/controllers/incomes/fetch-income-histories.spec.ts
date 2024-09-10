71b437f6c4823e192666bf7ce7c1456238b7f61b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-10 10:30:46 -0300

    test: create a test E2E for the controller to fetch the history of an income

---
+++ src/http/controllers/incomes/fetch-income-histories.spec.ts
@@ -0,0 +1,49 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { generateIncomes } from '@/utils/test/generate-incomes'
+import { registerAndAuthenticateUser } from '@/utils/test/register-and-authenticate'
+
+describe('Fetch income histories (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to fetch income histories by user', async () => {
+    const { user, token } = await registerAndAuthenticateUser(app)
+
+    const { incomes } = await generateIncomes({
+      userId: user.id,
+      amount: 1,
+      withCategory: true,
+      withIncomeHistories: {
+        min: 20,
+        max: 20,
+        disabled: false,
+      },
+    })
+
+    const response = await request(app.server)
+      .get(`/income-histories/${incomes[0].id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        count: 20,
+        next: 2,
+        previous: null,
+        page: 1,
+        total_pages: 2,
+        per_page: 10,
+        pagination_disabled: false,
+      }),
+    )
+  })
+})
